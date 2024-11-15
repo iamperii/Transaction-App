a02b08d5f0254f56d634427bee40a49fcc113bf9
Memmedova Peri <perimemmedova20052022@gmail>
2024-11-15 16:58:46 +0400

    addnewTransaction func created.

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // fetch('https://acb-api.algoritmika.org/api/transaction');
 
 const button = document.querySelector('.button-container');
-const boxes=document.querySelector(".boxes")
+const boxes = document.querySelector('.boxes');
+const module = document.querySelector('.module');
+
+let newTransaction = {
+	from: 'Peri',
+	to: 'Minure',
+	amount: 200,
+};
+async function deleteTransaction() {}
+async function editTrsansaction() {}
+async function addnewTransaction() {
+	let response = await fetch(
+		'https://acb-api.algoritmika.org/api/transaction',
+		{
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(newTransaction),
+		}
+	);
+	if (!response.ok) {
+		throw new Error(`HTTP error! Status: ${response.status}`);
+	}
+	let data = await response.json();
+	console.log(data);
+}
+
+button.addEventListener('click', addnewTransaction);
